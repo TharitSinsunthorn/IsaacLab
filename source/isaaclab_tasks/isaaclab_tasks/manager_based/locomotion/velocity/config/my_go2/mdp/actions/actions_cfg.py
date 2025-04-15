0da9c5df820998b10d1f363b2ed99c1d01db76e0
srl-limb-ws2 <sintharit1716@gmail.com>
2025-04-15 15:04:23 +0900

    try cpg modulator in joint position action

---
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/mdp/actions/actions_cfg.py
@@ -0,0 +1,32 @@
+from dataclasses import MISSING
+
+from isaaclab.controllers import DifferentialIKControllerCfg, OperationalSpaceControllerCfg
+from isaaclab.managers.action_manager import ActionTerm, ActionTermCfg
+from isaaclab.utils import configclass
+
+from . import cpg_joint_action
+from isaaclab.envs.mdp.actions import JointActionCfg
+
+
+##
+# Joint actions.
+##
+
+@configclass
+class CPGJointPositionActionCfg(JointActionCfg):
+    """Configuration for the joint position action term.
+
+    See :class:`JointPositionAction` for more details.
+    """
+
+    class_type: type[ActionTerm] = cpg_joint_action.CPGJointPositionAction
+
+    use_default_offset: bool = True
+    """Whether to use default joint positions configured in the articulation asset as offset.
+    Defaults to True.
+
+    If True, this flag results in overwriting the values of :attr:`offset` to the default joint positions
+    from the articulation asset.
+    """
+
+
