0adcbe2af05140e92426bda7b1faa05ea1c6b529
srl-limb-ws2 <sintharit1716@gmail.com>
2025-04-18 19:12:50 +0900

    separate lowg env to new file in my go2 config

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/__init__.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/__init__.py
@@ -89,7 +89,7 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:LowGravityUnitreeGo2RoughEnvCfg",
+        "env_cfg_entry_point": f"{__name__}.lowg_env_cfg:LowGravityUnitreeGo2RoughEnvCfg",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LowGravityUnitreeGo2RoughPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
     },
@@ -100,7 +100,7 @@ gym.register(
     entry_point="isaaclab.envs:ManagerBasedRLEnv",
     disable_env_checker=True,
     kwargs={
-        "env_cfg_entry_point": f"{__name__}.rough_env_cfg:LowGravityUnitreeGo2RoughEnvCfg_PLAY",
+        "env_cfg_entry_point": f"{__name__}.lowg_env_cfg:LowGravityUnitreeGo2RoughEnvCfg_PLAY",
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:LowGravityUnitreeGo2RoughPPORunnerCfg",
         "skrl_cfg_entry_point": f"{agents.__name__}:skrl_rough_ppo_cfg.yaml",
     },
