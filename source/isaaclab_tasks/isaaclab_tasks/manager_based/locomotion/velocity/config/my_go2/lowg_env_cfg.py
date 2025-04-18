b0ed99ecf48dda38396ad18cf52aded6efdef3b6
srl-limb-ws2 <sintharit1716@gmail.com>
2025-04-18 19:12:50 +0900

    separate lowg env to new file in my go2 config

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/lowg_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/lowg_env_cfg.py
@@ -5,7 +5,7 @@
 
 from isaaclab.utils import configclass
 
-from isaaclab_tasks.manager_based.locomotion.velocity.my_velocity_env_cfg import MyLocomotionVelocityRoughEnvCfg
+from isaaclab_tasks.manager_based.locomotion.velocity.low_G_env_cfg import LowGravityLocomotionVelocityRoughEnvCfg
 
 ##
 # Pre-defined configs
@@ -14,7 +14,7 @@ from isaaclab_assets.robots.unitree import UNITREE_GO2_CFG  # isort: skip
 
 
 @configclass
-class MyUnitreeGo2RoughEnvCfg(MyLocomotionVelocityRoughEnvCfg):
+class LowGravityUnitreeGo2RoughEnvCfg(LowGravityLocomotionVelocityRoughEnvCfg):
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
@@ -22,9 +22,6 @@ class MyUnitreeGo2RoughEnvCfg(MyLocomotionVelocityRoughEnvCfg):
         self.scene.robot = UNITREE_GO2_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
         self.scene.height_scanner.prim_path = "{ENV_REGEX_NS}/Robot/base"
         # scale down the terrains because the robot is small
-        self.scene.terrain.terrain_generator.sub_terrains["boxes"].grid_height_range = (0.025, 0.1)
-        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].noise_range = (0.01, 0.06)
-        self.scene.terrain.terrain_generator.sub_terrains["random_rough"].noise_step = 0.01
 
         # reduce action scale
         self.actions.joint_pos.scale = 0.25
@@ -47,44 +44,44 @@ class MyUnitreeGo2RoughEnvCfg(MyLocomotionVelocityRoughEnvCfg):
             },
         }
 
-        # rewards # default according to go2 setting
-        self.rewards.track_lin_vel_xy_exp.weight = 1.5 # default 1.5
+        # override rewards
+        # --task
+        self.rewards.track_lin_vel_xy_exp.weight = 2.0 # default 1.5
         self.rewards.track_ang_vel_z_exp.weight = 0.75 # default 0.75
-        self.rewards.feet_air_time.params["sensor_cfg"].body_names = ".*_foot"
         self.rewards.feet_air_time.weight = 0.125 # default 0.125
+        self.rewards.foot_clearance.weight = 0.5
+        self.rewards.feet_stance = None
         
+        # -- penalties
+        # body related
         self.rewards.lin_vel_z_l2.weight = -1.0 # default -2.0
-        self.rewards.ang_vel_xy_l2.weight = -0.0 # default -0.05
-        self.rewards.dof_torques_l2.weight = -1.0e-5 # default -1.0e-5
+        self.rewards.ang_vel_xy_l2 = None # default -0.05
+        self.rewards.flat_orientation_l2.weight = -1.0
+        self.rewards.body_lin_acc_l2 = None # -5.0e-4
+        # joint related
+        self.rewards.dof_torques_l2 = None # default -1.0e-5
         self.rewards.dof_acc_l2.weight = -2.5e-7 # default -2.5
-        self.rewards.action_rate_l2.weight = -0.01 # default -0.01
-        self.rewards.body_lin_acc_l2 = None # default -5.0e-4
-        self.rewards.flat_orientation_l2 = None
-
-        self.rewards.undesired_contacts = None
-        # self.rewards.undesired_contacts.params["sensor_cfg"].body_names = ".*_thigh"
-        # self.rewards.undesired_contacts.weight = -1.0 # default -1.0
-        self.rewards.contact_forces = None
-        # self.rewards.contact_forces.params["sensor_cfg"].body_names = ".*_foot"
-        # self.rewards.contact_forces.weight = -0.25 # default -0.25
         self.rewards.dof_pos_limits.weight = -1.0 # default 0.0
-
-        self.rewards.feet_contact_limit.weight = -0.15
-        self.rewards.feet_stance.weight = 0.2
-        # self.rewards.feet_slides.params["sensor_cfg"].body_names = ".*_foot"
-        # self.rewards.feet_slides.weight = -0.1 # default -0.1
-
+        self.rewards.action_rate_l2.weight = -0.005 # default -0.01
+        # foot related
+        self.rewards.undesired_contacts.weight = -1.0
+        self.rewards.contact_forces = None # default -0.25s
+        self.rewards.feet_contact_limit.weight = -0.2
+        self.rewards.foot_slip = None
+        self.rewards.swing_impact = None
+        
         # terminations
         self.terminations.base_contact.params["sensor_cfg"].body_names = "base"
 
 
 @configclass
-class MyUnitreeGo2RoughEnvCfg_PLAY(MyUnitreeGo2RoughEnvCfg):
+class LowGravityUnitreeGo2RoughEnvCfg_PLAY(LowGravityUnitreeGo2RoughEnvCfg):
     def __post_init__(self):
         # post init of parent
         super().__post_init__()
 
         # make a smaller scene for play
+        self.viewer.eye = [2.5, 2.5, 2.5]
         self.scene.num_envs = 50
         self.scene.env_spacing = 2.5
         # spawn the robot randomly in the grid (instead of their terrain levels)
