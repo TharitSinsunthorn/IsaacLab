f21aa7dda90642bb433f225516106a868152b858
srl-limb-ws2 <sintharit1716@gmail.com>
2025-06-11 18:03:21 +0900

    [SUCCESS] update CPG action

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/my_velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/my_velocity_env_cfg.py
@@ -127,36 +127,41 @@ class ActionsCfg:
             "FL": mdp_go2.CPGQuadrupedActionCfg.LegCfg(
                 joint_names=["FL_hip_joint", "FL_thigh_joint", "FL_calf_joint"],
                 body_name="FL_foot",
-                init_theta=np.random.uniform(-math.pi, math.pi),
-                hip_offset=(0.2, 0.142, 0.0)
+                # init_theta=np.random.uniform(-math.pi, math.pi),
+                init_theta=0.0,
+                hip_offset=(0.19, 0.142, 0.0)
             ),
             "FR": mdp_go2.CPGQuadrupedActionCfg.LegCfg(
                 joint_names=["FR_hip_joint", "FR_thigh_joint", "FR_calf_joint"],
                 body_name="FR_foot",
-                init_theta=np.random.uniform(-math.pi, math.pi),
-                hip_offset=(0.2, -0.142, 0.0)
+                # init_theta=np.random.uniform(-math.pi, math.pi),
+                init_theta=0.0,
+                hip_offset=(0.19, -0.142, 0.0)
             ),
             "RL": mdp_go2.CPGQuadrupedActionCfg.LegCfg(
                 joint_names=["RL_hip_joint", "RL_thigh_joint", "RL_calf_joint"],
                 body_name="RL_foot",
-                init_theta=np.random.uniform(-math.pi, math.pi),
-                hip_offset=(-0.23, 0.142, 0.0)
+                # init_theta=np.random.uniform(-math.pi, math.pi),
+                init_theta=0.0,
+                hip_offset=(-0.19, 0.142, 0.0)
             ),
             "RR": mdp_go2.CPGQuadrupedActionCfg.LegCfg(
                 joint_names=["RR_hip_joint", "RR_thigh_joint", "RR_calf_joint"],
                 body_name="RR_foot",
-                init_theta=np.random.uniform(-math.pi, math.pi),
-                hip_offset=(-0.23, -0.142, 0.0)
+                # init_theta=np.random.uniform(-math.pi, math.pi),
+                init_theta=0.0,
+                hip_offset=(-0.19, -0.142, 0.0)
             )
         },
         # You can override global CPG parameters here
         global_h=np.random.uniform(0.25,0.4), # Start height
-        global_gc=0.16, # Ground clearance for swing
+        global_gc=0.2, # Ground clearance for swing
         global_gp=0.02, # Ground penetration for stance
         global_d_step=0.4, # Step size scale
         mu_range=(1.0, 2.0), # RL agent can choose mu between 0.8 and 1.8
-        omega_range=(0.0, 15.0), # RL agent can choose frequency
-        scale = 1.0
+        omega_range=(0.0, 20.0), # RL agent can choose frequency
+        coupling_enable=False,
+        scale = 0.5
     )
 
 
