0260960c69ff5bfb30c7d127359783bff35a1b05
srl-limb-ws2 <sintharit1716@gmail.com>
2025-06-16 15:21:08 +0900

    update rsl rl config for low G locomotion

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/agents/rsl_rl_ppo_cfg.py
@@ -39,7 +39,7 @@ class UnitreeGo2RoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         entropy_coef=0.01,
         num_learning_epochs=5,
         num_mini_batches=4,
-        learning_rate=1.0e-3, # default 1.0e-3
+        learning_rate=1.0e-4, # default 1.0e-3
         schedule="adaptive",
         gamma=0.99,
         lam=0.95,
@@ -66,12 +66,23 @@ class LowGravityUnitreeGo2RoughPPORunnerCfg(RslRlOnPolicyRunnerCfg):
     save_interval = 50
     experiment_name = "unitree_go2_rough_lowG"
     empirical_normalization = False
-    policy = RslRlPpoActorCriticCfg(
+    # policy = RslRlPpoActorCriticCfg(
+    #     init_noise_std=1.0,
+    #     actor_hidden_dims=[512, 256, 128],
+    #     critic_hidden_dims=[512, 256, 128],
+    #     activation="elu",
+    # )
+    policy = RslRlPpoActorCriticRecurrentCfg(
         init_noise_std=1.0,
-        actor_hidden_dims=[512, 256, 128],
-        critic_hidden_dims=[512, 256, 128],
+        actor_hidden_dims=[256, 128],
+        critic_hidden_dims=[256, 128],
         activation="elu",
+        rnn_type="lstm",  # or "gru"
+        rnn_hidden_dim=512,  # hidden dimension of the RNN
+        rnn_num_layers=1,  # number of RNN layers
     )
+
+    clip_actions = 1.0
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
@@ -95,5 +106,5 @@ class LowGravityUnitreeGo2FlatPPORunnerCfg(LowGravityUnitreeGo2RoughPPORunnerCfg
 
         self.max_iterations = 1000 # dafault: 300
         self.experiment_name = "unitree_go2_flat_lowG"
-        self.policy.actor_hidden_dims = [128, 128, 128]
-        self.policy.critic_hidden_dims = [128, 128, 128]
+        # self.policy.actor_hidden_dims = [128, 128, 128]
+        # self.policy.critic_hidden_dims = [128, 128, 128]
