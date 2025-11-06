4527f553dc3d5299b78c0b1947e66d12432e3ffa
srl-limb-ws2 <sintharit1716@gmail.com>
2025-11-06 22:07:57 +0900

    add lunar go2 inference

---
+++ scripts/tutorials/03_envs/lunar_go2_inference.py
@@ -0,0 +1,91 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script demonstrates policy inference in a prebuilt USD environment.
+
+In this example, we use a locomotion policy to control the H1 robot. The robot was trained
+using Isaac-Velocity-Rough-H1-v0. The robot is commanded to move forward at a constant velocity.
+
+.. code-block:: bash
+
+    # Run the script
+    ./isaaclab.sh -p scripts/tutorials/03_envs/policy_inference_in_usd.py --checkpoint /path/to/jit/checkpoint.pt
+
+"""
+
+"""Launch Isaac Sim Simulator first."""
+
+
+import argparse
+
+from isaaclab.app import AppLauncher
+
+# add argparse arguments
+parser = argparse.ArgumentParser(description="Tutorial on inferencing a policy on an H1 robot in a warehouse.")
+parser.add_argument("--checkpoint", type=str, help="Path to model checkpoint exported as jit.", required=True)
+
+# append AppLauncher cli args
+AppLauncher.add_app_launcher_args(parser)
+# parse the arguments
+args_cli = parser.parse_args()
+
+# launch omniverse app
+app_launcher = AppLauncher(args_cli)
+simulation_app = app_launcher.app
+
+"""Rest everything follows."""
+import io
+import os
+import torch
+
+import omni
+
+from isaaclab.envs import ManagerBasedRLEnv
+from isaaclab.terrains import TerrainImporterCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+
+from isaaclab_rl.rsl_rl import RslRlVecEnvWrapper
+
+from isaaclab_tasks.manager_based.locomotion.velocity.config.my_go2.lowg_env_cfg import LowGravityUnitreeGo2RoughEnvCfg_PLAY
+
+
+def main():
+    """Main function."""
+    # load the trained jit policy
+    policy_path = os.path.abspath(args_cli.checkpoint)
+    file_content = omni.client.read_file(policy_path)[2]
+    file = io.BytesIO(memoryview(file_content).tobytes())
+    policy = torch.jit.load(file, map_location=args_cli.device)
+
+    # setup environment
+    env_cfg = LowGravityUnitreeGo2RoughEnvCfg_PLAY()
+    env_cfg.scene.num_envs = 1
+    env_cfg.curriculum = None
+    env_cfg.scene.terrain = TerrainImporterCfg(
+        prim_path="/World/ground",
+        terrain_type="usd",
+        usd_path=f"/home/srl-limb-ws2/Downloads/spacedata_moon_updated/spacedata_moon/moon_simplified.usd",
+    )
+    env_cfg.sim.device = args_cli.device
+    if args_cli.device == "cpu":
+        env_cfg.sim.use_fabric = False
+
+    # create environment
+    env_unwrapped = ManagerBasedRLEnv(cfg=env_cfg)
+    env = RslRlVecEnvWrapper(env_unwrapped, clip_actions=1.0)
+
+    # run inference with the policy
+    obs, _ = env.reset()
+    with torch.inference_mode():
+        while simulation_app.is_running():
+            action = policy(obs)
+            # action = torch.clamp(action, -1.0, 1.0)
+            obs, _, _, _ = env.step(action)
+
+
+if __name__ == "__main__":
+    main()
+    simulation_app.close()
