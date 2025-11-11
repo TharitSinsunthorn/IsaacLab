2d8255319f1c12b05e131e42071fd898a1a3f6d8
srl-limb-ws2 <sintharit1716@gmail.com>
2025-11-12 01:14:42 +0900

    add termination penalty

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/my_velocity_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/my_velocity_env_cfg.py
@@ -419,6 +419,10 @@ class RewardsCfg:
                 "threshold": 1.0
         },
     )
+    termination_penalty = RewTerm(
+        func=mdp.is_terminated,
+        weight=-400.0
+    )
     contact_forces = RewTerm(
         func=mdp_go2.body_frame_contact_force_z_penalty,
         weight=-0.25,
