89049173c04f561cf949b90e9edc41644f93fce7
srl-limb-ws2 <sintharit1716@gmail.com>
2026-02-17 15:35:51 +0900

    low g final weight tuning

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/lowg_env_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/my_go2/lowg_env_cfg.py
@@ -53,7 +53,7 @@ class LowGravityUnitreeGo2RoughEnvCfg(LowGravityLocomotionVelocityRoughEnvCfg):
         # --task
         self.rewards.track_lin_vel_xy_exp.weight = 2.0 # default 1.5
         self.rewards.track_ang_vel_z_exp.weight = 0.75 # default 0.75
-        self.rewards.feet_air_time.weight = 0.155 # default 0.125
+        self.rewards.feet_air_time.weight = 0.2 # default 0.125
         self.rewards.foot_clearance = None # default 0.1 previously None
         self.rewards.feet_stance = None
         self.rewards.crawl_reward = None
@@ -66,7 +66,7 @@ class LowGravityUnitreeGo2RoughEnvCfg(LowGravityLocomotionVelocityRoughEnvCfg):
         self.rewards.body_lin_acc_l2 = None # -5.0e-4
         # joint related
         self.rewards.dof_torques_l2 = None# default -1.0e-5
-        self.rewards.dof_acc_l2.weight = -2.5e-7 # default -2.5
+        self.rewards.dof_acc_l2.weight = -5.0e-7 # default -2.5
         self.rewards.dof_vel_l2 = None # default -0.01
         self.rewards.dof_pos_limits.weight = -1.0 # default 0.0
         self.rewards.action_rate_l2.weight = -0.01 # default -0.01
@@ -78,7 +78,7 @@ class LowGravityUnitreeGo2RoughEnvCfg(LowGravityLocomotionVelocityRoughEnvCfg):
         self.rewards.foot_slip.weight = -0.1 # default -0.1
         self.rewards.swing_impact = None # default -0.1
 
-        self.rewards.energy_consumption.weight = -0.01 # default -0.01
+        self.rewards.energy_scaling.weight = -0.01 # default -0.01
         self.rewards.gia_reward = None # default 0.05
         
         # terminations
@@ -111,6 +111,7 @@ class LowGravityUnitreeGo2RoughEnvCfg_PLAY(LowGravityUnitreeGo2RoughEnvCfg):
         # self.events.add_base_mass = None
         # self.events.physics_material = None
         # self.events.push_robot = None
+        self.events.randomize_gravity_on_reset = None
         
         # self.terminations.calf_contact = None
         # self.terminations.thigh_contact = None
